1da905ab3d7721334312a85acd7ef6381a20bf37
teidesu <me@tei.su>
2021-04-29 22:29:59 +0300

    feat(core): export BinaryReader and BinaryWriter

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,4 +5,8 @@ export * from './types'
 
 export * from './utils/crypto'
 export * from './utils/peer-utils'
+
+export { BinaryReader } from './utils/binary/binary-reader'
+export { BinaryWriter } from './utils/binary/binary-writer'
+
 export { defaultDcs } from './utils/default-dcs'
