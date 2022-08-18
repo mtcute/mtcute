e39057bda5d99c52c53926daa6a0af0a63c4d8d9
teidesu <86301490+teidesu@users.noreply.github.com>
2022-08-18 17:46:03 +0300

    fix(file-id): bumped version
    todo: should we really enforce the version? tdlib uses current version from Version.h, but not every version there changes file id format (and v32 is fully compatible with v41 as far as im concerned)

--- packages/file-id/src/types.ts
+++ packages/file-id/src/types.ts
@@ -7,7 +7,7 @@ export namespace tdFileId {
     export const WEB_LOCATION_FLAG = 1 << 24
     export const FILE_REFERENCE_FLAG = 1 << 25
 
-    export const CURRENT_VERSION = 32
+    export const CURRENT_VERSION = 41
 
     /**
      * An error occurred while parsing or serializing a File ID
