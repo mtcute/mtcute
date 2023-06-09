16392ee613f82900450e29f30c8f1103bdc54d70
Alina Sireneva <alina@tei.su>
2023-06-10 01:43:52 +0300

    build: use lint-staged + pre-commit prettier and type checking

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const packagesDir = path.join(__dirname, 'packages')
+
+module.exports = {
+    '*.{js,jsx,ts,tsx}': (filenames) => {
+        const modifiedPackages = new Set()
+
+        for (const filename of filenames) {
+            if (!filename.startsWith(packagesDir)) continue
+            modifiedPackages.add(filename.slice(packagesDir.length + 1).split(path.sep)[0])
+        }
+
+        return [
+            `prettier --write ${filenames.join(' ')}`,
+            `eslint --fix ${filenames.join(' ')}`,
+            ...[...modifiedPackages].map((pkg) => `pnpm -C packages/${pkg} build --noEmit`)
+        ]
+    }
+}
