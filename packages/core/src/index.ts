d4555f107edc9121641d8bace0a19660f1b91ab4
Alina Sireneva <alina@tei.su>
2023-12-04 15:40:17 +0300

    fix(core): apparently re-export from tl
    apparently due to some issue in vite `export *` from cjs module doesnt export shit

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,7 +5,7 @@ export * from './types/index.js'
 export * from './utils/peer-utils.js'
 
 /** @hidden */
-export * from '@mtcute/tl'
+export { mtp, tl } from '@mtcute/tl'
 import Long from 'long'
 /** @hidden */
 export { Long }
