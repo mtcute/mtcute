2eb4d307cbfe536021ab67f9224d1ba15dd4ec1e
Alina Sireneva <alina@tei.su>
2023-12-04 15:08:05 +0300

    fix(client): fixed typings

--- packages/client/build.config.cjs
+++ packages/client/build.config.cjs
@@ -1,17 +1,3 @@
-module.exports = ({ fs, path, outDir }) => ({
+module.exports = {
     esmOnlyDirectives: true,
-    final() {
-        function fixClient(file) {
-            // make TelegramClient a class, not an interface
-            const dTsContent = fs.readFileSync(path.join(outDir, file), 'utf8')
-
-            fs.writeFileSync(
-                path.join(outDir, file),
-                dTsContent.replace('export interface TelegramClient', 'export class TelegramClient'),
-            )
-        }
-
-        fixClient('esm/client.d.ts')
-        fixClient('cjs/client.d.ts')
-    },
-})
+}
