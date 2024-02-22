f05ada6cd26da2d5e447aec46912ca626c91f32c
Alina Sireneva <alina@tei.su>
2024-02-22 14:20:23 +0300

    fix(create-bot): use `cross-spawn`
    i hate windows lol

--- packages/create-bot/src/utils.ts
+++ packages/create-bot/src/utils.ts
@@ -1,14 +1,16 @@
 import chalk from 'chalk'
-import * as cp from 'child_process'
+import { spawn } from 'cross-spawn'
 
 export function exec(cwd: string, ...cmd: string[]) {
     return new Promise<void>((resolve, reject) => {
         console.log(`${chalk.blue('$')} ${cmd.join(' ')}`)
 
-        const proc = cp.spawn(cmd[0], cmd.slice(1), {
+        const proc = spawn(cmd[0], cmd.slice(1), {
             stdio: 'inherit',
             cwd,
         })
+
+        proc.on('error', reject)
 
         proc.on('close', (code) => {
             if (code === 0) {
