46414e4e19456b4d252d288b9ab6a4065cdc5be9
alina sireneva <alina@tei.su>
2025-01-19 20:15:16 +0300

    fix(wasm): remove vite-ignore annotation from result

---
+++ docs/.vitepress/theme/vue.d.ts
@@ -0,0 +1,7 @@
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+
+    // eslint-disable-next-line ts/no-empty-object-type
+    const component: DefineComponent<{}, {}, any>
+    export default component
+}
--- packages/wasm/build.config.js
+++ packages/wasm/build.config.js
@@ -2,11 +2,21 @@ import * as fs from 'node:fs'
 import { resolve } from 'node:path'
 
 /** @type {import('@fuman/build/vite').CustomBuildConfig} */
-export default () => ({
-    preparePackageJson({ packageJson }) {
-        packageJson.exports['./mtcute.wasm'] = './mtcute.wasm'
-    },
-    finalize({ packageDir, outDir }) {
-        fs.cpSync(resolve(packageDir, 'src/mtcute.wasm'), resolve(outDir, 'mtcute.wasm'))
-    },
-})
+export default () => {
+    return {
+        pluginsPre: [
+            {
+                name: 'remove-vite-ignore',
+                renderChunk(code) {
+                    return code.replace('/* @vite-ignore */', '')
+                },
+            },
+        ],
+        preparePackageJson({ packageJson }) {
+            packageJson.exports['./mtcute.wasm'] = './mtcute.wasm'
+        },
+        finalize({ packageDir, outDir }) {
+            fs.cpSync(resolve(packageDir, 'src/mtcute.wasm'), resolve(outDir, 'mtcute.wasm'))
+        },
+    }
+}
