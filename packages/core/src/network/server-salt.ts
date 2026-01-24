58a95504029a6b93298b89ecda6226f99d21ede2
alina sireneva <alina@tei.su>
2026-01-24 19:01:02 +0300

    fix(core): correctly handle expiring server salts

--- packages/core/src/network/server-salt.ts
+++ packages/core/src/network/server-salt.ts
@@ -19,18 +19,28 @@ export class ServerSaltManager {
     // todo: we should use adjusted monotonic clock here
     const now = Date.now() / 1000
 
-    while (salts.length > 0 && now > salts[0].validSince) {
+    while (salts.length > 0 && now > salts[0].validUntil) {
       this.currentSalt = salts[0].salt
       this._futureSalts.shift()
     }
 
-    if (!this._futureSalts.length) this.currentSalt = Long.ZERO
-    else this._scheduleNext()
+    if (!this._futureSalts.length) {
+      this.currentSalt = Long.ZERO
+    } else {
+      this._scheduleReplace(this._futureSalts[0])
+    }
   }
 
   private _timer?: timers.Timer
 
-  private _scheduleNext(): void {
+  private _scheduleReplace(salt: mtp.RawMt_future_salt): void {
+    this._timer = timers.setTimeout(() => {
+      this.currentSalt = salt.salt
+      this._replaceAndScheduleNext()
+    }, salt.validSince * 1000 - Date.now())
+  }
+
+  private _replaceAndScheduleNext(): void {
     if (this._timer) timers.clearTimeout(this._timer)
 
     const now = Date.now() / 1000
@@ -38,11 +48,12 @@ export class ServerSaltManager {
     // find the first valid salt from the stored ones
     let next: mtp.RawMt_future_salt | undefined
     while (this._futureSalts.length !== 0) {
-      const salt = this._futureSalts.shift()!
+      const salt = this._futureSalts[0]
       if (salt.validSince <= now && now <= salt.validUntil) {
         next = salt
         break
       }
+      this._futureSalts.shift()
     }
 
     if (!next) {
@@ -50,13 +61,7 @@ export class ServerSaltManager {
       return
     }
 
-    this._timer = timers.setTimeout(
-      () => {
-        this.currentSalt = next.salt
-        this._scheduleNext()
-      },
-      next.validSince * 1000 - Date.now(),
-    )
+    this._scheduleReplace(next)
   }
 
   destroy(): void {
