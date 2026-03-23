33bde9c4c666bc3aca7b18a65094264057af94a0
alina sireneva <alina@tei.su>
2026-03-23 12:51:19 +0300

    fix(core): force a release

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,7 @@ import Long from 'long'
 export * from './highlevel/index.js'
 export * from './network/index.js'
 export * from './storage/index.js'
+
 /** @hidden */
 export { mtp, tl } from './tl/index.js'
 export * from './types/index.js'
